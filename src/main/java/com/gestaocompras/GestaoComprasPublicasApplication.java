faca2273cc43255e9b7d8ef665b940505fd3f18e
Enzo Barbosa <pasquatty@gmail.com>
2026-08-23 00:47:04 -0300

    add: base of project

---
+++ src/main/java/com/gestaocompras/GestaoComprasPublicasApplication.java
@@ -0,0 +1,13 @@
+package com.gestaocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoComprasPublicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoComprasPublicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestaocompras/GestaoComprasPublicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestaocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoComprasPublicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
